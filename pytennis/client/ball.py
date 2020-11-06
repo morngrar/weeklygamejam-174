f11a0d607797ce2c0a8141151b3fc7fc7c4567a2
charleyv <charleyv@stud.ntnu.no>
2020-11-06 21:30:42 +0100

    Added ball class

--- pytennis/client/ball.py
+++ pytennis/client/ball.py
@@ -3,11 +3,10 @@ import pygame
 
 class Ball:
     def __init__(self):
-        self.x = 0
-        self.y = 0
-        self.width = 20
-        self.height = 20
+        self.x = 640
+        self.y = 400
+        self.radius = 10
         self.ball_color = (220, 253, 80)
 
     def draw(self, window):
-        pygame.draw.ellipse(window, self.ball_color, self)
+        pygame.draw.ellipse(window, self.ball_color, (x - radius, y - radius), radius)
--- pytennis/client/client.py
+++ pytennis/client/client.py
@@ -1,9 +1,14 @@
 # Made with python 3.8
 
-import pygame
+import pygame, sys
+from client import ball
 
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 800
+BALL_WIDTH = 20
+BALL_HEIGHT = 20
+PLAYER_WIDTH = 30
+PLAYER_HEIGHT = 30
 
 window = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Tennis game")
@@ -16,6 +21,13 @@ def redrawGameWindow():
 def main():
     pass
 
+tennis_ball = ball.Ball()
+
+ball = pygame.Rect(SCREEN_WIDTH/2 - (BALL_WIDTH/2), SCREEN_HEIGHT/2 - (BALL_HEIGHT/2) ,BALL_WIDTH,BALL_HEIGHT)
+player = pygame.Rect(SCREEN_WIDTH - (PLAYER_WIDTH/2), (SCREEN_HEIGHT/2) - (PLAYER_HEIGHT/2), PLAYER_WIDTH, PLAYER_HEIGHT)
+
+
+court_color = (0,133,102)
 
 """ main loop """
 run = True
@@ -25,14 +37,13 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+            pygame.quit
+            sys.exit()
 
+    # Visuals
+    tennis_ball.draw()
 
-
-
-
-
-
-
+    
     # Update screen
     redrawGameWindow()
 """ main loop end """
