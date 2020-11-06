5db4416a19e08d3ec8a23ba2319ed2f00627bb05
charleyv <charleyv@stud.ntnu.no>
2020-11-06 19:20:06 +0100

    Ball vel, player velocity

--- common/common.py
+++ common/common.py
@@ -5,6 +5,10 @@ class GameState:
         self.opponentPos = [OPPONENT_START_X, OPPONENT_START_Y, RACKET_HEIGHT]
         self.ownPos = [OWN_START_X, OWN_START_Y, RACKET_HEIGHT]
         self.ballPos = [0, 0, 0]
+        self.ballVelocity = [0, 0, 0]
         self.opponentPoints = 0
         self.ownPoints = 0
         self.gameOver = False
+        self.ownRacketTilt = 1.0
+        self.ownRacketVelocity = [0,0]
+        
