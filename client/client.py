8788deb32e5a7626edcdf25baff8f2a69dbe1995
Joakim Vetle Berg <joakivb@stud.ntnu.no>
2020-11-06 20:19:11 +0100

    temp

--- client/client.py
+++ client/client.py
@@ -2,31 +2,40 @@
 
 import pygame
 
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 800
+
+window = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Tennis game")
+clock = pygame.time.Clock()
+
+
+def redrawGameWindow():
+    pygame.display.update()
+
 def main():
     pass
 
 
+""" main loop """
+run = True
+while run:
+    clock.tick(27)
 
-
-# def gameloop: 
-#     conn = createServerConnection()
-
-#     while True:
-
-
-#         gamestate = conn.getGameState()
-
-#         gamestate = {
-#             "opponentPos" : 
-#         }
-
-
-#         render()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            rul = False
 
 
 
 
 
+
+
+
+    # Update screen
+    redrawGameWindow()
+""" main loop end """
 
 
 if __name__=="__main__":
