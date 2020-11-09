c5595db14fd6b04f00f7e422f60888e7456d7d99
Svein-Kåre Bjørnsen <sveinkare@gmail.com>
2020-11-09 17:03:01 +0100

    enabled domain

--- pytennis/common/networking.py
+++ pytennis/common/networking.py
@@ -5,7 +5,7 @@ This module has common functionality regarding networking for both the client an
 
 
 # host and port
-HOST = "127.0.0.1" #"tennis.alepou.no"      # can be any IPv4 address or valid hostname
+HOST = "tennis.alepou.no"      # can be any IPv4 address or valid hostname
 PORT = 59338            # must be between 1024 and 65535
 
 MAX_PACKET_SIZE = 1024  # max number of bytes that will be sent through a connection at a time
